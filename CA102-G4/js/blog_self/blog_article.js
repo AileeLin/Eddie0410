553d56e518bbaffc0c560439780dda29c4ee971e
EddieHuang <99407071@stud.sju.edu.tw>
2018-07-04 22:54:49 +0800

    2018/07/04_V24
    1. 更新blog_article.html 左邊文章區和留言板、右邊作者最近文章區
    2. 更新blog_article.js、blog_article.css

--- CA102-G4/js/blog_self/blog_article.js
+++ CA102-G4/js/blog_self/blog_article.js
@@ -1,33 +1,13 @@
-$(function(){
-	$(".tnbutton.follow").tooltip();
-	$(".tnbutton.follow").unbind("click.follow").bind("click.follow",function(){
-		var me = this, gp = $(me).parent(".follow_btn_group"),
-			isfollowed = $(me).hasClass("unfollow"),mid=gp.attr("data-target_mid"),mnick=gp.attr("data-target_mnick");
-		if($("#userid").val()>0){
-			if($(me).hasClass("doing")) return;
-			$(me).addClass("doing");
-			$.ajax({
-        		type: 'POST',
-        		url: site_path + "member/" + (isfollowed?'unfollow':'follow'),
-        		data: {following_id: mid},
-        		success: function(data) {
-					$(me).removeClass("doing");
-                    if( data == 1 ){
-                		$(me).addClass("displaynone").siblings(".follow").removeClass("displaynone");
-                        $('#follower_cnt_'+mid).html(parseInt($('#follower_cnt_'+mid).html())+(isfollowed?-1:1));			
-						createToast('success','您'+(isfollowed?'取消關注':'關注了')+mnick+'。');
-                    }else if(data == 0){ 
-                		$(me).addClass("displaynone").siblings(".follow").removeClass("displaynone");
-    				}else{
-    					alert(data);
-    				}
-        		},
-          	   error: function(data) { 
-					$(me).removeClass("doing");            		
-               }
-        	});
-		}else{
-    		$('#form_signin').submit();
-    	}
-	});
+$(document).ready(function () {
+
+    $(".col-4 > div.ui.basic.buttons > button:nth-child(2)").click(function () {
+        if ($(".collection").css('font-weight') == 900) {
+            $(".collection").css('font-weight', '400');
+            $(".collection").css('color', 'black');
+        } else {
+            $(".collection").css('font-weight', '900');
+            $(".collection").css('color', 'red');
+        }
+    });
+
 });
